e3175a672db21f0900a8ced2abdbf7ff42e43a52
Anatolii Platonov <platonoff.dev@gmail.com>
2025-07-08 23:36:45 +0200

    Add gset lab solution

---
+++ g-set/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+type task struct {
+	f  func() error
+	dt time.Duration
+}
+
+type ReplicateMessage struct {
+	maelstrom.MessageBody
+	Value []int `json:"value"`
+}
+
+type AddMessage struct {
+	maelstrom.MessageBody
+	Element int `json:"element"`
+}
+
+func main() {
+	l := log.New(os.Stderr, "TASK: ", log.Default().Flags())
+	n := maelstrom.NewNode()
+	var neighbors []string
+
+	gset := newSet()
+
+	tasks := []task{
+		{
+			dt: 5 * time.Second,
+			f: func() error {
+				for _, neighbor := range n.NodeIDs() {
+					err := n.Send(
+						neighbor,
+						map[string]any{
+							"type":  "replicate",
+							"value": gset.serialize(),
+						},
+					)
+					if err != nil {
+						l.Println("Send failed: ", err)
+					}
+				}
+
+				return nil
+			},
+		},
+	}
+
+	for _, t := range tasks {
+		go func() {
+			for {
+				err := t.f()
+				if err != nil {
+					l.Println("Task error: ", err)
+				}
+				time.Sleep(t.dt)
+			}
+		}()
+	}
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topology := body["topology"].(map[string]any)
+		topologyNeighbours := topology[n.ID()].([]any)
+		for _, neighbor := range topologyNeighbours {
+			neighbors = append(neighbors, neighbor.(string))
+		}
+
+		return n.Reply(msg, map[string]any{
+			"type": "topology_ok",
+		})
+	})
+
+	n.Handle("add", func(msg maelstrom.Message) error {
+		var body AddMessage
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		gset.add(body.Element)
+
+		return n.Reply(msg, map[string]any{
+			"type": "add_ok",
+		})
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		return n.Reply(msg, map[string]any{
+			"type":  "read_ok",
+			"value": gset.serialize(),
+		})
+	})
+
+	n.Handle("replicate", func(msg maelstrom.Message) error {
+		var body ReplicateMessage
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		gset.union(deserialize(body.Value))
+		return nil
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ g-set/set.go
@@ -0,0 +1,52 @@
+package main
+
+import "sync"
+
+type setItem interface {
+	int
+}
+
+type set[T setItem] struct {
+	mutex sync.RWMutex
+	m     map[T]any
+}
+
+func newSet[T setItem]() set[T] {
+	return set[T]{
+		mutex: sync.RWMutex{},
+		m:     map[T]any{},
+	}
+}
+
+func (s *set[T]) union(newSet set[T]) {
+	for newSetItem := range newSet.m {
+		s.add(newSetItem)
+	}
+}
+
+func (s *set[T]) serialize() []T {
+	result := []T{}
+	s.mutex.Lock()
+	for item := range s.m {
+		result = append(result, item)
+	}
+	s.mutex.Unlock()
+
+	return result
+}
+
+func deserialize[T setItem](items []T) set[T] {
+	newSet := newSet[T]()
+
+	for _, item := range items {
+		newSet.add(item)
+	}
+
+	return newSet
+}
+
+func (s *set[T]) add(element T) {
+	s.mutex.Lock()
+	s.m[element] = true
+	s.mutex.Unlock()
+}
