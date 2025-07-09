f23f99ce2634464d343fbbc2784b8e11bdc0ca1e
Anatolii Platonov <platonoff.dev@gmail.com>
2025-07-09 02:35:00 +0200

    Add pn-counter implementation

---
+++ pn-counter/counter.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"sync"
+)
+
+type pncounter struct {
+	mutex     *sync.Mutex
+	pCounters map[string]int
+	nCounters map[string]int
+}
+
+func newCounter() pncounter {
+	return pncounter{
+		mutex:     &sync.Mutex{},
+		pCounters: map[string]int{},
+		nCounters: map[string]int{},
+	}
+}
+
+func (c *pncounter) add(nodeID string, delta int) {
+	c.mutex.Lock()
+
+	if delta > 0 {
+		counter := c.pCounters[nodeID]
+		c.pCounters[nodeID] = counter + delta
+	} else {
+		counter := c.nCounters[nodeID]
+		c.nCounters[nodeID] = counter + delta
+	}
+
+	c.mutex.Unlock()
+}
+
+func (c *pncounter) read() int {
+	s := 0
+	c.mutex.Lock()
+	for _, v := range c.pCounters {
+		s += v
+	}
+
+	for _, v := range c.nCounters {
+		s -= v
+	}
+
+	c.mutex.Unlock()
+	return s
+}
+
+func (c *pncounter) merge(externalCounter pncounter) {
+	c.mutex.Lock()
+
+	for node, counter := range externalCounter.pCounters {
+		c.pCounters[node] = max(c.pCounters[node], counter)
+	}
+
+	for node, counter := range externalCounter.nCounters {
+		c.nCounters[node] = min(c.nCounters[node], counter)
+	}
+
+	c.mutex.Unlock()
+}
+
+func (c *pncounter) serialize() any {
+	return c.pCounters
+}
+
+func deserialize(data any) pncounter {
+	counter := newCounter()
+	counter.pCounters = data.(map[string]int)
+	return counter
+}
