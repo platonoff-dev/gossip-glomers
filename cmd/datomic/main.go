dcadf66920d8d8d4b475518e84785451ff8b4c2e
Anatolii Platonov <platonoff.dev@gmail.com>
2025-07-19 19:53:09 +0200

    Initial txn implementation

--- cmd/datomic/main.go
+++ cmd/datomic/main.go
@@ -1,13 +1,122 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
+	"os"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type Transaction struct {
+	Op     string
+	Values []int
+	Key    int
+}
+
+type TransactionRequest struct {
+	maelstrom.MessageBody
+	Transactions [][]any `json:"txn"`
+}
+
+func deserializeTrasactions(raw [][]any) ([]Transaction, error) { //nolint: gocognit
+	l := log.New(os.Stderr, "DESERIALIZE: ", log.Default().Flags())
+	l.Println(raw)
+	transactions := []Transaction{}
+	for _, t := range raw {
+		l.Println(t)
+		op, ok := t[0].(string)
+		if !ok {
+			return nil, errors.New("bad transaction format: op")
+		}
+
+		key, ok := t[1].(float64)
+		if !ok {
+			return nil, errors.New("bad transaction format: key")
+		}
+
+		values := []int{}
+		if t[2] != nil { //nolint: nestif
+			rawValues, isList := t[2].([]any)
+			rawValue, isNumber := t[2].(float64)
+			if !isList && !isNumber {
+				return nil, errors.New("bad transaction format: values")
+			}
+
+			if isNumber {
+				values = append(values, int(rawValue))
+			}
+
+			if isList {
+				for _, v := range rawValues {
+					numValue, ok := v.(float64)
+					if !ok {
+						return nil, errors.New("bad transaction format: values")
+					}
+
+					values = append(values, int(numValue))
+				}
+			}
+		}
+
+		transactions = append(transactions, Transaction{
+			Op:     op,
+			Key:    int(key),
+			Values: values,
+		})
+	}
+
+	return transactions, nil
+}
+
+func serializeTransactions(transactions []Transaction) [][]any {
+	l := log.New(os.Stderr, "SERIALIZE: ", log.Default().Flags())
+	result := [][]any{}
+
+	for _, t := range transactions {
+		l.Println(t)
+		var resultValue any
+		if t.Op == "append" {
+			resultValue = t.Values[0]
+		} else {
+			resultValue = t.Values
+		}
+
+		result = append(result, []any{
+			t.Op,
+			t.Key,
+			resultValue,
+		})
+	}
+
+	return result
+}
+
 func main() {
 	n := maelstrom.NewNode()
+
+	n.Handle("txn", func(msg maelstrom.Message) error {
+		var body TransactionRequest
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		transactions, err := deserializeTrasactions(body.Transactions)
+		if err != nil {
+			return err
+		}
+
+		result := serializeTransactions(transactions)
+		if err != nil {
+			return err
+		}
+
+		return n.Reply(msg, map[string]any{
+			"type": "txn_ok",
+			"txn":  result,
+		})
+	})
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
